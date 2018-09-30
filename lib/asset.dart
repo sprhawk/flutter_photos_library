e90c7414f6c953ebf7dc960df3866fc959849698
sprhawk <465558+sprhawk@users.noreply.github.com>
2018-09-30 17:48:10 +0800

    use assetView to load assets' thumbnails

--- lib/asset.dart
+++ lib/asset.dart
@@ -1,26 +1,9 @@
-import 'package:flutter/services.dart';
-import 'photos_library.dart';
+import 'dart:typed_data';
 
 class Asset {
   final String identifier;
   final int width;
   final int height;
-  ByteData imageData;
 
-  String channelName;
-
-  Asset({this.identifier, this.width, this.height}) {
-    const prefix = 'flutter.yang.me/photos_library/image';
-    this.channelName = prefix + '/' + this.identifier;
-  }
-
-  void requestThumbnail(int width, int height, void handler(ByteData imageData)) {
-    BinaryMessages.setMessageHandler(channelName, (message) {
-      this.imageData = message;
-      handler(message);
-      // BinaryMessages.setMessageHandler(channelName, null);
-    });
-
-    PhotosLibrary.requestThumbnail(this.identifier, width, height);
-  }
+  Asset({this.identifier, this.width, this.height});
 }
--- lib/assetview.dart
+++ lib/assetview.dart
@@ -1,6 +1,8 @@
 import 'package:meta/meta.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'asset.dart';
+import 'photos_library.dart';
 
 class AssetView extends StatefulWidget {
   final int index;
@@ -23,24 +25,48 @@ class AssetState extends State<AssetView> {
   Asset asset;
   final double width;
   final double height;
+
+  ByteData _imageData;
+  String _channelName;
+
   AssetState(
-      {@required this.index, @required this.asset, this.width, this.height});
+      {@required this.index, @required this.asset, this.width, this.height}) {
+    const prefix = 'flutter.yang.me/photos_library/image';
+    this._channelName = prefix + '/' + this.asset.identifier;
+  }
 
   @override
   void initState() {
     super.initState();
 
-    this.asset.requestThumbnail(this.width.toInt(), this.height.toInt(),
-        (imageData) {
-      setState(() {});
+    this._requestThumbnail(
+        width: this.width.toInt(),
+        height: this.height.toInt());
+  }
+
+  @override
+  void deactivate() {
+    super.deactivate();
+    BinaryMessages.setMessageHandler(this._channelName, null);
+  }
+
+  void _requestThumbnail(
+      {int width, int height}) {
+    BinaryMessages.setMessageHandler(this._channelName, (message) {
+      this._imageData = message;
+      setState(() {
+              
+            });
     });
+
+    PhotosLibrary.requestThumbnail(this.asset.identifier, width, height);
   }
 
   @override
   Widget build(BuildContext context) {
-    if (null != this.asset.imageData) {
+    if (null != this._imageData) {
       var image = Image.memory(
-        this.asset.imageData.buffer.asUint8List(),
+        this._imageData.buffer.asUint8List(),
         fit: BoxFit.cover,
         width: this.width,
         height: this.height,
